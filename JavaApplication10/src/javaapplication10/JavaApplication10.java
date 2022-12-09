b3381f3331c9a48d8346850ebecc521a6ad2fde0
Ascencio <jascencio21@alumnos.utalca.cl>
2022-12-09 11:38:32 -0300

    sd

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -6,95 +6,188 @@ public class JavaApplication10 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m=0, key, begin, end;
-        String opcion, item;
-        while (m!=2) {
-            System.out.print("\nOpcion 0: AVL\nOpcion 1: SkipList\nOpcion 2: Salir\nIngrese opcion: ");
-            m = sc.nextInt();
-            if (m == 0) {
-                AVL avl = new AVL();
-                opcion = "";
-                while(!"e".equals(opcion)){
-                    System.out.print("\nOpcion i: insert\nOpcion d: remove\nOpcion f: find\nOpcion r: findRange\nOpcion h: height\nOpcion s: toStrig\nOpcion e: exit\nIngrese opcion: ");
-                    opcion = sc.next();
-                    if ("i".equals(opcion)) {
-                        System.out.print("Ingrese key: ");
-                        key = sc.nextInt();
-                        System.out.print("Ingrese item: ");
-                        item = sc.next();
-                        avl.insert(key, item);
-                    } else if ("d".equals(opcion)) {
-                        System.out.print("Ingrese key: ");
-                        key = sc.nextInt();
-                        avl.remove(key);
-                    } else if ("f".equals(opcion)) {
-                        System.out.print("Ingrese key: ");
-                        key = sc.nextInt();
-                        AVL aux = avl.find(key);
-                        if(aux != null){
-                            System.out.println(aux.head.data + ", " + aux.head.key);
-                        }
-                    } else if ("r".equals(opcion)) {
-                        System.out.print("Ingrese begin: ");
-                        begin = sc.nextInt();
-                        System.out.print("Ingrese end: ");
-                        end = sc.nextInt();
-                        avl.findRange(begin, end);
-                    } else if ("h".equals(opcion)) {
-                        System.out.print("Ingrese key: ");
-                        key = sc.nextInt();
-                        avl.heigth(key);
-                    } else if ("s".equals(opcion)) {
-                        System.out.println(avl.toString());
-                    } else if("e".equals(opcion)){
-                    }else{
-                        System.out.println("Opcion incorrecta");
+        AVL avl = new AVL();
+        SkipList sl = new SkipList();
+        int type;
+        String opcion;
+        type = Integer.parseInt(args[0]);
+        opcion = args[1];
+        if ("s".equals(opcion)) {
+//            sc = new Scanner("C:\\Users\\PC GAMER\\Desktop\\A\\"+args[2]);
+//            //leer archivo
+//            String v = "";
+//            if (args.length > 3) {
+//                v = args[3];
+//            }
+//            while (true) {
+//                int key;
+//                String data, opt;
+//                opt = sc.next();
+//                key = sc.nextInt();
+//                data = "0";
+//                if (!opt.equals("d") && !opt.equals("f")) {
+//                    data = sc.next();
+//                }
+//
+//                if (opt.charAt(0) == '0' && data.charAt(0) == '0' && key == 0) {
+//                    break;
+//                }
+//
+//                if (type == 0) {
+//                    switch (opt) {
+//                        case "i":
+//                            avl.insert(key, data);
+//                            if ("v".equals(v)) {
+//                                System.out.println("Insertado: " + key);
+//                            }
+//                            break;
+//                        case "d":
+//                            avl.remove(key);
+//                            if (avl.head == null) {
+//                                avl = new AVL();
+//                            }
+//                            if ("v".equals(v)) {
+//                                System.out.println("removido: " + key);
+//                            }
+//                            break;
+//                        case "f":
+//                            AVL aux = avl.find(key);
+//                            if (aux != null) {
+//                                System.out.println(aux.head.data + ", " + aux.head.key);
+//                            }
+//                            if ("v".equals(v)) {
+//                                System.out.println("fin");
+//                            }
+//                            break;
+//                        case "r":
+//                            avl.findRange(key, Integer.parseInt(data));
+//                            if ("v".equals(v)) {
+//                                System.out.println("fin");
+//                            }
+//                            break;
+//                    }
+//
+//                } else {
+//                    switch (opt) {
+//                        case "i":
+//                            sl.insert(key, data);
+//                            if ("v".equals(v)) {
+//                                System.out.println("Insertado: " + key);
+//                            }
+//                            break;
+//                        case "d":
+//                            sl.remove(key);
+//                            if ("v".equals(v)) {
+//                                System.out.println("removido: " + key);
+//                            }
+//                            break;
+//                        case "f":
+//                            if (sl.find(key).key == key) {
+//                                System.out.println(sl.find(key).item);
+//                            }
+//                            if ("v".equals(v)) {
+//                                System.out.println("fin");
+//                            }
+//                            break;
+//                        case "r":
+//                            sl.findRange(key, Integer.parseInt(data));
+//                            if ("v".equals(v)) {
+//                                System.out.println("fin");
+//                            }
+//                            break;
+//                    }
+//                }
+//
+//            }
+        }
+
+        if ("i".equals(opcion)) {
+            String v = "";
+            if (args.length > 2) {
+                v = args[2];
+            }
+            while (true) {
+                int key;
+                String data, opt;
+                opt = sc.next();
+                key = sc.nextInt();
+                data = "0";
+                if (!opt.equals("d") && !opt.equals("f")) {
+                    data = sc.next();
+                }
+
+                if (opt.charAt(0) == '0' && data.charAt(0) == '0' && key == 0) {
+                    break;
+                }
+
+                if (type == 0) {
+                    switch (opt) {
+                        case "i":
+                            avl.insert(key, data);
+                            if ("v".equals(v)) {
+                                System.out.println("Insertado: " + key);
+                            }
+                            break;
+                        case "d":
+                            avl.remove(key);
+                            if (avl.head == null) {
+                                avl = new AVL();
+                            }
+                            if ("v".equals(v)) {
+                                System.out.println("removido: " + key);
+                            }
+                            break;
+                        case "f":
+                            AVL aux = avl.find(key);
+                            if (aux != null) {
+                                System.out.println(aux.head.data + ", " + aux.head.key);
+                            }
+                            if ("v".equals(v)) {
+                                System.out.println("fin");
+                            }
+                            break;
+                        case "r":
+                            avl.findRange(key, Integer.parseInt(data));
+                            if ("v".equals(v)) {
+                                System.out.println("fin");
+                            }
+                            break;
+                    }
+
+                } else {
+                    switch (opt) {
+                        case "i":
+                            sl.insert(key, data);
+                            if ("v".equals(v)) {
+                                System.out.println("Insertado: " + key);
+                            }
+                            break;
+                        case "d":
+                            sl.remove(key);
+                            if ("v".equals(v)) {
+                                System.out.println("removido: " + key);
+                            }
+                            break;
+                        case "f":
+                            if (sl.find(key).key == key) {
+                                System.out.println(sl.find(key).item);
+                            }
+                            if ("v".equals(v)) {
+                                System.out.println("fin");
+                            }
+                            break;
+                        case "r":
+                            sl.findRange(key, Integer.parseInt(data));
+                            if ("v".equals(v)) {
+                                System.out.println("fin");
+                            }
+                            break;
                     }
                 }
-            } else if (m == 1) {
-                SkipList sl = new SkipList();
-                opcion = "";
-                while (!"e".equals(opcion)) {
-                    System.out.print("\nOpcion i: insert\nOpcion d: remove\nOpcion f: find\nOpcion r: findRange\nOpcion h: height\nOpcion s: toStrig\nOpcion e: exit\nIngrese opcion: ");
-                    opcion = sc.next();
-                    if ("i".equals(opcion)) {
-                        System.out.print("Ingrese key: ");
-                        key = sc.nextInt();
-                        System.out.print("Ingrese item: ");
-                        item = sc.next();
-                        sl.insert(key, item);
-                    } else if ("d".equals(opcion)) {
-                        System.out.print("Ingrese key: ");
-                        key = sc.nextInt();
-                        sl.remove(key);
-                    } else if ("f".equals(opcion)) {
-                        System.out.print("Ingrese key: ");
-                        key = sc.nextInt();
-                        if(sl.find(key).key == key){
-                            System.out.println(sl.find(key).item);
-                        }
-                    } else if ("r".equals(opcion)) {
-                        System.out.print("Ingrese begin: ");
-                        begin = sc.nextInt();
-                        System.out.print("Ingrese end: ");
-                        end = sc.nextInt();
-                        sl.findRange(begin, end);
-                    } else if ("h".equals(opcion)) {
-                        System.out.print("Ingrese key: ");
-                        key = sc.nextInt();
-                        System.out.println(sl.height(key));
-                    } else if ("s".equals(opcion)) {
-                        System.out.println(sl.toString());
-                    } else if("e".equals(opcion)){
-                    }else{
-                        System.out.println("Opcion incorrecta");
-                    }
-                }
-            } else if(m == 2){
-            }else{
-                System.out.println("Opcion incorrecta");
+
             }
         }
+
     }
 
 }
